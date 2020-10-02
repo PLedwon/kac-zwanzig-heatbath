01ba9764126c560eaea7b8c4c03d7f9d2948d3e8
PLedwon <paul.ledwon@protonmail.com>
2020-10-02 10:40:04 +0200

    sync

--- plots.py
+++ plots.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import math
 import scipy
 
-resultList = glob.glob('*.np[yz]')
+resultList = glob.glob('/users/stud/ledwon/Documents/npzFiles/*.np[yz]')
 
 data=np.load(resultList[0])
 varQ=np.zeros(np.size(data['squaredQ']))
@@ -47,15 +47,16 @@ def memoryKernel(times):
         return result
 
 def theoDiff(times,gamma):
-        return np.power(times,gamma) * varQ[math.floor((t1/dt)*0.3)]/np.power(timesteps[math.floor((t1/dt)*0.3)],gamma)
+        return np.power(times,gamma) * varQ[startindex]/np.power(timesteps[startindex],gamma)
 
 
 
-startindex = math.floor((t1/dt)*0.2)
-const = varQ[math.floor((t1/dt)*0.3)]/np.power(timesteps[math.floor((t1/dt)*0.3)],gamma)
+startindex = int(math.floor((t1/dt)*0.1))
+endindex = int(math.floor(t1/dt))
+const = varQ[startindex]/np.power(timesteps[startindex],gamma)
 #const_control = varQ[math.floor((t1/dt)*0.3)]/np.power(ensemble1.timesteps[math.floor((t1/dt)*0.3)],1.5)
 var = plt.figure(1)
-plt.plot(timesteps,varQ,timesteps[math.floor((t1/dt)*0.1):math.floor(t1/dt)],theoDiff(timesteps,gamma)[math.floor((t1/dt)*0.1):math.floor(t1/dt)])
+plt.loglog(timesteps,varQ,timesteps[startindex:endindex],theoDiff(timesteps,gamma)[startindex:endindex])
 #plt.loglog(ensemble1.timesteps,ensemble1.varQ,ensemble1.timesteps[math.floor((t1/dt)*0.1):math.floor(t1/dt)],theoDiff(ensemble1.timesteps,const,gamma)[math.floor((t1/dt)*0.1):math.floor(t1/dt)],ensemble1.timesteps[math.floor((t1/dt)*0.1):math.floor(t1/dt)])#,theoDiff(ensemble1.timesteps,const,1.4)[math.floor((t1/dt)*0.1):math.floor(t1/dt)])
 #plt.hist(ensemble.singleBath.q[0,:],1000)
 plt.xlabel('t')
@@ -63,11 +64,11 @@ plt.ylabel('Var(Q)')
 var.savefig("varQ.pdf",bbox_inches='tight')
 
 
-K = plt.figure(2)
-plt.plot(timesteps,memoryKernel(timesteps))
+Kernel = plt.figure(2)
+plt.plot(timesteps,memoryKernel(timesteps),timesteps,K)
 plt.xlabel('t')
 plt.ylabel('Memory Kernel')
-K.savefig("K.pdf",bbox_inches='tight')
+Kernel.savefig("K.pdf",bbox_inches='tight')
 
 #plt.figure(3)
 #plt.plot(timesteps,ensemble1.singleBath.Q)
