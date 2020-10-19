9411363ba203ce7def8cdb5d165bf0a365ed1ed0
PLedwon <paul.ledwon@protonmail.com>
2020-10-19 18:08:44 +0200

    sync stuff

--- plots.py
+++ plots.py
@@ -53,19 +53,19 @@ def memoryKernel(times):
         result *= 1.0/np.sum(result)
         return result
 
-def theoDiff(times,gamma):
+def theoDiff(times, gamma, fitindex):
         return np.power(times,gamma) * varQ[startindex]/np.power(timesteps[startindex],gamma)
 
 
 
 startindex = int(math.floor((t1/dt)*0.20))
 endindex = int(math.floor(t1/dt))
-mid = int(endindex*0.95)
+fitindex = int(endindex*0.95)
 
-const = varQ[mid]/np.power(timesteps[mid],gamma)
+#const = varQ[mid]/np.power(timesteps[mid],gamma)
 #const_control = varQ[math.floor((t1/dt)*0.3)]/np.power(ensemble1.timesteps[math.floor((t1/dt)*0.3)],1.5)
 var = plt.figure(1)
-plt.loglog(timesteps[startindex:endindex:16000],varQ[startindex:endindex:16000],':',timesteps[startindex:endindex:16000],theoDiff(timesteps,gamma)[startindex:endindex:16000])
+plt.loglog(timesteps[startindex:endindex:16000],varQ[startindex:endindex:16000],':',timesteps[startindex:endindex:16000],theoDiff(timesteps,gamma,fitindex)[startindex:endindex:16000])
 #plt.loglog(ensemble1.timesteps,ensemble1.varQ,ensemble1.timesteps[math.floor((t1/dt)*0.1):math.floor(t1/dt)],theoDiff(ensemble1.timesteps,const,gamma)[math.floor((t1/dt)*0.1):math.floor(t1/dt)],ensemble1.timesteps[math.floor((t1/dt)*0.1):math.floor(t1/dt)])#,theoDiff(ensemble1.timesteps,const,1.4)[math.floor((t1/dt)*0.1):math.floor(t1/dt)])
 #plt.hist(ensemble.singleBath.q[0,:],1000)
 plt.xlabel('t')
