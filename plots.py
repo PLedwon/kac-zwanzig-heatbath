fa925ef61f4a3a0cd197af0430b042de9d3964bc
PLedwon <paul.ledwon@protonmail.com>
2020-10-19 19:24:57 +0200

    sync stuff

--- plots.py
+++ plots.py
@@ -58,9 +58,9 @@ def theoDiff(times, gamma, fitindex):
 
 
 
-startindex = int(math.floor((t1/dt)*0.20))
+startindex = int(math.floor((t1/dt)*0.50))
 endindex = int(math.floor(t1/dt))
-fitindex = int(endindex*0.2)
+fitindex = startindex#int(endindex*0.2)
 
 #const = varQ[mid]/np.power(timesteps[mid],gamma)
 #const_control = varQ[math.floor((t1/dt)*0.3)]/np.power(ensemble1.timesteps[math.floor((t1/dt)*0.3)],1.5)
