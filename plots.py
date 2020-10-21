2444de2cbbfe4a23be5a818bf26ccb7db94c27b9
PLedwon <paul.ledwon@protonmail.com>
2020-10-21 12:34:22 +0200

    sync stuff

--- plots.py
+++ plots.py
@@ -59,7 +59,7 @@ def theoDiff(times, gamma, fitindex):
 
 
 startindex = int(math.floor((t1/dt)*0.10))
-endindex = int(math.floor(t1/dt)*0.6)
+endindex = int(math.floor(t1/dt)*0.7)
 fitindex = int(math.floor((t1/dt)*0.2))
 
 var = plt.figure(1)
