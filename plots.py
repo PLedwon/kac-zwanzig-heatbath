86b987638783c6fb948f8e36156d0ea2eb4a6440
PLedwon <paul.ledwon@protonmail.com>
2020-11-03 11:11:29 +0100

    sync stuff

--- plots.py
+++ plots.py
@@ -120,14 +120,14 @@ linpopt, linpcov = curve_fit(linDiff, timesteps[endindex::2000],varQ[endindex::2
 print(popt)
 
 var = plt.figure(1)
+plt.xscale('log', nonposx="clip")
+plt.yscale('log', nonposy="clip")
 plt.errorbar(timesteps[startindex::8000],varQ[startindex::8000],label='Numerical results')
 plt.errorbar(timestepsErr, varQ[timeToIndexArray], std)
 plt.errorbar(timesteps[startindex:endindex:8000],theoDiff(timesteps[startindex:endindex:8000],popt[0],popt[1]), color='#0066FF',linestyle='--',label=r'$\propto t^{1.5}$')
 plt.errorbar(timesteps[linindex::80000],linDiff(timesteps[linindex::80000],linpopt[0],linpopt[1]),linestyle=':',color='#009900',label=r'$\propto t$')
 plt.xlabel('t')
 plt.ylabel('Var(Q)')
-plt.xscale('log')
-plt.yscale('log', nonposy="clip")
 plt.legend()
 var.savefig("./img/varQlog.pdf",bbox_inches='tight')
 
