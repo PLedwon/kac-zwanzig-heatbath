1481a1927e5e068e3d4a1a15e641cfd031c3cb89
PLedwon <paul.ledwon@protonmail.com>
2020-11-03 10:34:28 +0100

    sync stuff

--- plots.py
+++ plots.py
@@ -131,7 +131,7 @@ var.savefig("./img/varQlog.pdf",bbox_inches='tight')
 
 var = plt.figure(2)
 plt.plot(timesteps[::8000],varQ[::8000],label='Numerical results',color='#FC9169' )
-plt.errorbar(timestepsErr, varQ[timeToIndexArray], std, fmt='none', ecolor='#FC9169',elinewidth='1.0')
+plt.errorbar(timestepsErr, varQ[timeToIndexArray], std, fmt='none', ecolor='#FC9169',elinewidth='0.5')
 #plt.plot(timesteps[startindex:endindex:8000],theoDiff(timesteps,gamma,fitindex)[startindex:endindex:8000],label=r'$\propto t^{1.5}$')
 plt.plot(timesteps[startindex:endindex:8000],theoDiff(timesteps[startindex:endindex:8000],popt[0],popt[1]),label=r'$\propto t^{1.5}$',color='#0066FF', linestyle='--')
 plt.plot(timesteps[linindex::80000],linDiff(timesteps[linindex::80000],linpopt[0],linpopt[1]),label=r'$\propto t$', linestyle=':',color='#009900')
