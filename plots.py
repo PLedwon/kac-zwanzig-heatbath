e1d10c031f703c878f08a8a0b0b1f37778886ffd
PLedwon <paul.ledwon@protonmail.com>
2020-11-16 14:39:45 +0100

    sync stuff

--- plots.py
+++ plots.py
@@ -126,7 +126,7 @@ def linDiff(x,a,c):
 
 startindex = int(math.floor((t1/dt)*0.10))
 plotindex = int(math.floor((t1/dt)*0.05))
-kernelplotindex = int(math.floor((t1/dt)*0.5))
+kernelplotindex = int(math.floor((t1/dt)*0.2))
 endindex = int(math.floor(t1/dt)*0.7)
 linindex = int(math.floor(t1/dt)*0.8)
 #fitindex = int(math.floor((t1/dt)*0.2))
