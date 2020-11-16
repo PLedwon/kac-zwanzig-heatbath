b798b5b6095b3b242ec6fb4ca4cbed09df5b8e70
PLedwon <paul.ledwon@protonmail.com>
2020-11-16 13:12:19 +0100

    sync stuff

--- plots.py
+++ plots.py
@@ -94,7 +94,7 @@ else:
 
 #log
     #timestepsErrLog=np.power(10,np.log10(timesteps)[::indexSkipValue])
-    timestepsErrLog=np.logspace(np.log10(timesteps[0]),np.log10(timesteps[-indexSkipValue]) , num=errorbarCount)
+    timestepsErrLog=np.logspace(1.0,np.log10(timesteps[-indexSkipValue]) , num=errorbarCount)
     print(timestepsErrLog)
     timeToIndexArrayLog=np.floor(1.0/dt*timestepsErrLog)
     timeToIndexArrayLog=timeToIndexArrayLog.astype(int)
