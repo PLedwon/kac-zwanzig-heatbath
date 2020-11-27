93500c87b4b4424fe3dc2705c02378bcee22c889
PLedwon <paul.ledwon@protonmail.com>
2020-11-27 09:13:19 +0100

    sync stuff

--- plots.py
+++ plots.py
@@ -164,7 +164,7 @@ plt.legend()
 var.savefig("./img/varQ.pdf",bbox_inches='tight')
 
 
-kerneltimes=np.logspace(timesteps[kernelplotindex],timesteps[-1],1000)
+kerneltimes=np.logspace(np.log10(timesteps[kernelplotindex]),np.log10(timesteps[-1]),1000)
 Kernel = plt.figure(3)
 plt.ticklabel_format(axis="x", style="sci", scilimits=(0,0), useMathText=True)
 plt.ticklabel_format(axis="y", style="sci", scilimits=(0,0), useMathText=True)
